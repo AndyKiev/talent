314851a97be0720644954d0afd74eb0446432b47
Andrey <andrey.bakulin@gmail.com>
2026-04-17 11:43:49 +0300

    install  dependencies tanstack query, tanstack router, axios, zod, zustand

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv, type Plugin } from 'vite'
 import react from '@vitejs/plugin-react'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default (mode: string) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return defineConfig({
+    plugins: [
+      tanstackRouter({
+        target: 'react',
+        autoCodeSplitting: true,
+      }) as unknown as Plugin,
+      react(),
+    ] as Plugin[],
+    server: {
+      port: 4002,
+      host: '0.0.0.0',
+      proxy: {
+        '/api': {
+          target: env.VITE_BACKEND_API_URL,
+          changeOrigin: true,
+          secure: false,
+          rewrite: path => path.replace(/^\/api/, ''),
+        },
+      },
+    },
+  })
+}
